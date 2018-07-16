bb0d89c219a725c0899c1915480b8f6c91e1ad76
mauriceoc <maurice.oconnor@gmail.com>
2018-07-16 16:10:09 +0100

    create-links

--- create-links.sh
+++ create-links.sh
@@ -6,9 +6,9 @@ echo "=== CREATE LINKS ==="
 
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/pgclirc ~/.pgclirc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig:
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/tmux ~/.tmux
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux ~/.tmux
+ln -s ~/dotfiles/vim ~/.vim
 
 

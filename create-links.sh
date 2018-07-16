349178d68600b041b116a4bc8f1d9c7501f3bb58
mauriceoc <maurice.oconnor@gmail.com>
2018-07-16 16:17:12 +0100

    add dirs

--- create-links.sh
+++ create-links.sh
@@ -8,7 +8,7 @@ ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/pgclirc ~/.pgclirc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/tmux ~/.tmux
-ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/tmux/ ~/.tmux
+ln -s ~/dotfiles/vim/  ~/.vim
 
 

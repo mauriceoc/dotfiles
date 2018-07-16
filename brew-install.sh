e6dbabe7511844e7f289116f759531b206c6f290
mauriceoc <maurice.oconnor@gmail.com>
2018-07-16 16:24:24 +0100

    bugfix

--- brew-install.sh
+++ brew-install.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "=== HOMEBREW STUFF ==="
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install pgcli homebrew ag vim bash-completion nvm bash git reattach-to-user-namespace
+brew install pgcli ag vim bash-completion nvm bash git reattach-to-user-namespace
 

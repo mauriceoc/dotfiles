8bd0c4677184dc65fa6d8ba5315036993a212acc
mauriceoc <maurice.oconnor@gmail.com>
2018-07-16 16:39:00 +0100

    add bash profile

--- create-links.sh
+++ create-links.sh
@@ -7,6 +7,7 @@ set -e
 DOT_FILES=$HOME/dotfiles
 
 ln -s $DOT_FILES/bashrc $HOME/.bashrc
+ln -s $DOT_FILES/bash_profile $HOME/.bash_profile
 ln -s $DOT_FILES/pgclirc $HOME/.pgclirc
 ln -s $DOT_FILES/gitconfig $HOME/.gitconfig
 ln -s $DOT_FILES/tmux/tmux.conf $HOME/.tmux.conf

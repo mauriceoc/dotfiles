b141ceb9782695e1c92e0dc991168b54ffdffcba
mauriceoc <maurice.oconnor@gmail.com>
2018-07-16 16:47:28 +0100

    script updates

--- create-links.sh
+++ create-links.sh
@@ -5,15 +5,15 @@ echo "=== CREATE LINKS ==="
 set -e
 set -x
 
-DOT_FILES=$HOME/dotfiles
+DOT_FILES=${HOME}/dotfiles
 
-ln -s $DOT_FILES/bashrc $HOME/.bashrc
-ln -s $DOT_FILES/bash_profile $HOME/.bash_profile
-ln -s $DOT_FILES/pgclirc $HOME/.pgclirc
-ln -s $DOT_FILES/gitconfig $HOME/.gitconfig
-ln -s $DOT_FILES/tmux/tmux.conf $HOME/.tmux.conf
+ln -s ${DOT_FILES}/bashrc ${HOME}/.bashrc
+ln -s ${DOT_FILES}/bash_profile ${HOME}/.bash_profile
+ln -s ${DOT_FILES}/pgclirc ${HOME}/.pgclirc
+ln -s ${DOT_FILES}/gitconfig ${HOME}/.gitconfig
+ln -s ${DOT_FILES}/tmux/tmux.conf ${HOME}/.tmux.conf
 
-ln -s $DOT_FILES/tmux/ $HOME/.tmux
-ln -s $DOT_FILES/vim/  $HOME/.vim
+ln -s ${DOT_FILES}/tmux/ ${HOME}/.tmux
+ln -s ${DOT_FILES}/vim/  ${HOME}/.vim
 
 

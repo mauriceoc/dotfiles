aca36f540fe391ff151a1a32b3abd5a30cae0f31
mauriceoc <maurice.oconnor@gmail.com>
2018-07-17 14:41:25 +0100

    new stuff

--- git-install.sh
+++ git-install.sh
@@ -6,5 +6,7 @@ set -x
 echo "=== CLONE GIT REPOS ==="
 
 cd repos
+
 git clone https://github.com/tomislav/osx-terminal.app-colors-solarized.git
+git clone https://github.com/mrzool/bash-sensible.git
 

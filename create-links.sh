04fd616b868dd4c945fa90c1c287f1b9e941a3c5
mauriceoc <maurice.oconnor@gmail.com>
2018-07-23 13:14:03 +0100

    extra opt

--- create-links.sh
+++ create-links.sh
@@ -7,18 +7,18 @@ set -x
 
 DOT_FILES=${HOME}/dotfiles
 
-ln -sf ${DOT_FILES}/bashrc ${HOME}/.bashrc
-ln -sf ${DOT_FILES}/bash_profile ${HOME}/.bash_profile
-ln -sf ${DOT_FILES}/pgclirc ${HOME}/.pgclirc
-ln -sf ${DOT_FILES}/gitconfig ${HOME}/.gitconfig
-ln -sf ${DOT_FILES}/tmux/tmux.conf ${HOME}/.tmux.conf
+ln -shf ${DOT_FILES}/bashrc ${HOME}/.bashrc
+ln -shf ${DOT_FILES}/bash_profile ${HOME}/.bash_profile
+ln -shf ${DOT_FILES}/pgclirc ${HOME}/.pgclirc
+ln -shf ${DOT_FILES}/gitconfig ${HOME}/.gitconfig
+ln -shf ${DOT_FILES}/tmux/tmux.conf ${HOME}/.tmux.conf
 
-ln -sf ${DOT_FILES}/tmux/ ${HOME}/.tmux
-ln -sf ${DOT_FILES}/vim/  ${HOME}/.vim
+ln -shf ${DOT_FILES}/tmux/ ${HOME}/.tmux
+ln -shf ${DOT_FILES}/vim/  ${HOME}/.vim
 
 NVIM_CONFIG=${HOME}/.config/nvim
 
 mkdir -p ${NVIM_CONFIG}
 
-ln -sf ${DOT_FILES}/init.vim ${NVIM_CONFIG}/init.vim
+ln -shf ${DOT_FILES}/init.vim ${NVIM_CONFIG}/init.vim
 

1892b033982765339fd99590e1348363c50a4d97
moc <maurice@macbot.com>
2019-03-03 13:58:44 +0000

    add to gitignore

--- git-install.sh
+++ git-install.sh
@@ -8,9 +8,11 @@ echo "=== CLONE GIT REPOS ==="
 mkdir -p repos \
 && cd repos \
 && git clone https://github.com/tomislav/osx-terminal.app-colors-solarized.git \
-&& git clone https://github.com/mrzool/bash-sensible.git
+&& git clone https://github.com/mrzool/bash-sensible.git \
+&& cd ..
 
 mkdir -p vim/bundle \
 && cd vim/bundle \
-&& git clone git://github.com/godlygeek/tabular.git
+&& git clone git://github.com/godlygeek/tabular.git \
+&& cd ..
 

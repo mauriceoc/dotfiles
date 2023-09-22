6f3559cd3e1be9421485c0d91f0a5c0c47d22072
moc <maurice@macbot.com>
2023-09-22 23:33:58 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ ln -shf ${DOT_FILES}/tmux.conf ${HOME}/.tmux.conf
 ln -shf ${DOT_FILES}/init.vim ${NVIM_CONFIG}/init.vim
 
 # dirs
-ln -shf ${DOT_FILES}/tmux ${HOME}/.tmux
-ln -shf ${DOT_FILES}/vim  ${HOME}/.vim
-ln -shf ${DOT_FILES}/grc  ${HOME}/.grc
-
+ln -shf "${DOT_FILES}/tmux" "${HOME}/.tmux"
+ln -shf "${DOT_FILES}/vim"  "${HOME}/.vim"
+ln -shf "${DOT_FILES}/grc"  "${HOME}/.grc"
+ln -shf "${DOT_FILES}/fish" "${HOME}/.config/fish"
